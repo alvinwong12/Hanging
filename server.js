52868b01f6c5d22781055c081e8dc35d4ef810f1
Sidharth Thapar <thapar.si@husky.neu.edu>
2018-02-03 21:59:46 -0500

    serverjs expressjs

---
+++ server.js
@@ -0,0 +1,41 @@
+var app = require('./express');
+var bodyParser = require('body-parser');
+// var cookieParser = require('cookie-parser');
+// var session      = require('express-session');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// app.use(cookieParser());
+// app.use(session({ secret: "put some text here" })); // Store secret in environment variable
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// configure a public directory to host static content
+// app.use(app.express.static(__dirname + '/public'));
+
+// app.get('/env', function(req, res) {
+//     var connectionString = 'blah';
+//     if(process.env.MLAB_WEBDEV_NEU_UNAME) { // check if running remotely
+//         var username = process.env.MLAB_WEBDEV_NEU_UNAME; // get from environment
+//         var password = process.env.MLAB_WEBDEV_NEU_PASS;
+//         connectionString = 'mongodb://' + username + ':' + password;
+//         connectionString += '@ds139761.mlab.com:39761/heroku_v7g71n9h'; // user yours
+//     }
+
+//     res.json({connectionString: connectionString});
+// });
+
+// require("./assignment/app");
+
+
+app.post("/rest/register", function (req, res) {
+    res.send("Hello from service")
+});
+
+var port = process.env.PORT || 8000;
+
+app.listen(port, function(req,res){
+    console.log("running")
+});
