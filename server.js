bdcd3ac973f162733686304835e513f7de8c4c04
Sidharth Thapar <thapar.si@husky.neu.edu>
2018-02-04 09:09:32 -0500

    post user

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require('express')
 var app = express();
 var database = require('./utils/database')
+var bodyParser = require('body-parser');
 
-PORT = 3000
+PORT = 8000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
 	data = { "id": "RandomId",
   "username": "MyUserName",
   "location": { "latitude": 37.785834, "longitude": -122.406417 },
-  "status" : "Hey there! I am hanging" 
+  "status" : "Hey there! I am hanging"
 }
 	database.write(data, "users", res)
 })
@@ -22,6 +26,18 @@ app.get('/nearby', function(req,res){
 	database.findNearBy([data.location.latitude, data.location.longitude], 10000, res)
 })
 
-app.listen(3000, function(){
+app.post("/rest/submit", function (req, res) {
+    var user = req.body;
+    data = { "id": user.id,
+        "username": user.id,
+        "location": { "latitude": user.id.location.latitude, "longitude": user.id.location.longitude },
+        "status" : user.id.status
+    }
+    console.log(user)
+    database.write(data, "users", res)
+    // res.send("Hello from service")
+});
+
+app.listen(PORT, function(){
 	console.log("listening")
 })
