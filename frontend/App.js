4b47a090b7a6cbb2a279c472f59096c814d625e0
Tarun Khanna <tennispro1213@gmail.com>
2018-02-04 10:16:23 -0500

    updaate a lot

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Button, TextInput } from 'react-native';
+import { Image, KeyboardAvoidingView, StyleSheet, Text, View, FlatList, Button, TextInput } from 'react-native';
+
+// import { Icon } from 'react-native-vector-icons';
+// import EStyleSheet from 'react-native-extended-stylesheet';
+// EStyleSheet.build({ "$outline": 1 });
 
 const styles = require('./styles.js');
 const DEFAULT_STATUS = "Hey there! I am hanging";
+const DUMMY_RESPONSE = {
+  data: [
+    {
+      id: "Sample_Random_Id_1",
+      username: "Lacie P",
+      location: {
+        latitude: "lat",
+        longitude: "long",
+      },
+      status: "Freaking out about a wedding",
+    },
+    {
+      id: "Sample_Random_Id_2",
+      username: "Naomi",
+      location: {
+        latitude: "lat",
+        longitude: "long",
+      },
+      status: "Going to the beach!!!",
+    },
+    {
+      id: "Sample_Random_Id_3",
+      username: "Ryan",
+      location: {
+        latitude: "lat",
+        longitude: "long",
+      },
+      status: "Just bored on my way home. Looking to contemplate the meaning of life and avoid the spiral of darkness that follows.",
+    },
+  ],
+};
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,11 +49,12 @@ export default class App extends React.Component {
       latitude: null,
       longitude: null,
       error: null,
-      list: [{ key: 'a' }, { key: 'b' }],
+      list: DUMMY_RESPONSE.data,
       status: DEFAULT_STATUS,
+      refreshing: false,
     }
-
   }
+
   componentDidMount() {
     navigator.geolocation.getCurrentPosition((position) => {
       this.setState({
@@ -26,12 +62,12 @@ export default class App extends React.Component {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
         error: null,
-      })
+      });
     })
   }
 
   sendLocation() {
-    fetch('http://localhost:8000/fake/hanging', {
+    fetch('http://localhost:8000/rest/submit', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -48,35 +84,101 @@ export default class App extends React.Component {
       }),
     }).then((response) => response.json())
       .then((responseJson) => {
-        this.setState({
-          ...this.state,
-          list: [{ key: 5 }, { key: 7 }, {key : responseJson.username}, ],
-        })
-      }).catch((error) => console.log(error))
+        console.log(responseJson);
+        return responseJson
+      }).catch((error) => console.log("error", error))
   }
 
-  OnPressRefresh() {
+  OnPressUpdate() {
     this.sendLocation();
-    
+    this.setState({
+      ...this.state,
+      list: DUMMY_RESPONSE.data,
+    })
+  }
+
+  handleRefresh = () => {
+    console.log("HANDLE RESFRESH HANDLE RESFRESH HANDLE RESFRESH HANDLE RESFRESH HANDLE RESFRESH HANDLE RESFRESH ");
+    this.setState({
+      ...this.setState,
+      refreshing: true,
+    });
+    this.setState({
+      ...this.state,
+      list: [...this.state.list,
+      {
+        id: "Sample_Random_Id_4",
+        username: "Robert",
+        location: {
+          latitude: "lat",
+          longitude: "long",
+        },
+        status: "Going to lunch at ${insert potential sponsor}"
+      }],
+    });
+
+    fetch('http://localhost:8000/nearby')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        
+        this.setState({
+          ...this.state,
+          list: [...this.state.list,
+            ...responseJson
+          ],
+        });
+
+        return responseJson
+      }).catch((error) => {
+        console.log("error", error)
+      }).finally(() => {
+        this.setState({
+          ...this.setState,
+          refreshing: false,
+        });
+      });
+
   }
 
   render() {
-    
+
     return (
       <View style={styles.container}>
-        <TextInput placeholder={this.state.status} style={styles.input} onChangeText={(text) => this.setState({ text })} />
+        <Image
+          source={require('./images/hanging.png')}
+          style={{ height: 50 }}
+          resizeMode="contain"
+        />
+
+        <Text style={{ fontSize: 22, textAlign: "left", width: "100%", fontFamily: "Verdana", marginVertical: 10 }}>Potential friends available:</Text>
         <FlatList
           style={styles.list}
           data={this.state.list}
-          renderItem={({ item }) => <Text>{item.key}</Text>}
+          keyExtractor={(item, index) => index}
+          refreshing={this.state.refreshing}
+          onRefresh={this.handleRefresh}
+          renderItem={({ item }) => (
+            <View style={styles.listItem}>
+              <Text style={styles.listItemUsername}>{item.username}</Text>
+              <Text style={styles.listItemStatus}>{item.status}</Text>
+            </View>
+          )}
+          ItemSeparatorComponent={() => (<View style={{ height: 1 + StyleSheet.hairlineWidth, backgroundColor: "#ff9a91", margin: 5 }} />)}
         />
-        <Button
-          style={styles.refreshBtn}
-          onPress={() => this.OnPressRefresh()}
-          title="Refresh"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
+        <KeyboardAvoidingView behavior="padding" style={{ flexDirection: 'row', backgroundColor: "#ff9a91", borderRadius: 4, borderWidth: 0, }}>
+          {/* <View style={{ flexDirection: 'row' }}> */}
+          <TextInput placeholder={this.state.status} style={styles.input} onChangeText={(text) => this.setState({ text })} />
+
+          <Button
+            onPress={() => this.OnPressUpdate()}
+            title="Update"
+            color="#fff"
+            accessibilityLabel="Update Status"
+          />
+          {/* </View> */}
+        </KeyboardAvoidingView>
+
       </View>
     );
   }
--- frontend/styles.js
+++ frontend/styles.js
@@ -4,36 +4,75 @@ const { StyleSheet } = React
 
 const styles = StyleSheet.create({
     container: {
-        paddingLeft: 20,
-        paddingRight: 20,
-        paddingVertical: 20,
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'stretch',
-        justifyContent: 'center',
-    },
-    input: {
-     
-        height: 40,
-        backgroundColor: '#fff',
-        borderRadius: 4,
-        borderWidth: 0.5,
-        borderColor: '#d6d7da',
-        color: "#f00",
         alignItems: 'center',
         justifyContent: 'center',
+        paddingHorizontal: 10,
+        paddingVertical: 30,
+        width: "100%",
     },
     list: {
         flex: 1,
         backgroundColor: '#fff',
-        // alignItems: 'stretch',
-        // justifyContent: 'center',
+        width: "100%",
+        paddingVertical: 5,
+        
+    },
+    listItem: {
+        flex: 1,
+        
+        // lineHeight: 40,
+        // color: "blue",
+        // backgroundColor: 'black',
+        justifyContent: 'flex-start',
+        // textAlign: "left",
+    },
+    listItemUsername: {
+        flex: 1,
+        
+        fontSize: 20,
+        fontWeight: 'bold',
+        
+        color: "#ff9a91",
+        // backgroundColor: 'black',
+        // justifyContent: 'flex-end',
+        textAlign: "left",
+        fontFamily: "Verdana",
+    },
+    listItemStatus: {
+        flex: 1,
+        
+        // lineHeight: 40,
+        fontStyle: 'italic',
+        // color: "white",
+        // backgroundColor: 'black',
+        // justifyContent: 'flex-end',
+        textAlign: "left",
+        marginVertical: 5,
+        fontFamily: "Verdana",
+        
+    },
+    input: {
+     
+        height: 40,
+        flex: 3,
+        // width: "100%",
+        backgroundColor: '#fff',
+        // backgroundColor: 'green',
+        borderColor: 'transparent',
+        color: "#ff9a91",
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     refreshBtn: {
         flex: 1,
-        backgroundColor: '#fff',
+        height: 40,
+        backgroundColor: '#000',
+        // backgroundColor: '#00f',
         alignItems: 'center',
         justifyContent: 'center',
+        fontFamily: "Verdana",
     },
 });
 
